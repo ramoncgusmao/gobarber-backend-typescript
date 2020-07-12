dd910c10d081cacfc7533bf242aa9f931a65694e
ramon gusmao <ramoncgusmao@gmail.com>
2020-07-12 13:37:50 -0300

    colocando cors para poder acessar a web

--- src/repositories/AppointmentRepository.ts
+++ src/repositories/AppointmentRepository.ts
@@ -1,22 +1,18 @@
-
 import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentRepository extends Repository<Appointment>{
-    private appointments: Appointment[];
+class AppointmentRepository extends Repository<Appointment> {
+  private appointments: Appointment[];
 
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
-    public async findByDate(date: Date): Promise<Appointment | null>{
-
-        const findAppointment = await this.findOne({
-            where: {date},
-        })
-
-        return findAppointment || null;
-    }
-
+    return findAppointment || null;
+  }
 }
 
 export default AppointmentRepository;
