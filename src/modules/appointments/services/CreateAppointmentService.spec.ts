a6bbf7838edb423f246f5935bdd894b52833f928
ramon gusmao <ramoncgusmao@gmail.com>
2020-08-01 10:22:16 -0300

    comecando a criar tests utilizando o jest

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('should be able to create two appointment on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
